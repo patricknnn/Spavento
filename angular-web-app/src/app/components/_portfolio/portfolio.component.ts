c33e5ce390997ef9beff3bf500131179c6f48325
patricknnn <patrickniewold@gmail.com>
2020-07-18 15:16:00 +0200

    Updated styles

--- angular-web-app/src/app/components/_portfolio/portfolio.component.ts
+++ angular-web-app/src/app/components/_portfolio/portfolio.component.ts
@@ -8,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class PortfolioComponent implements OnInit {
   title = 'Portfolio';
   subTitle = 'An overview of my work.';
+  images: string[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.images = [
+      'https://w.wallhaven.cc/full/39/wallhaven-3911w9.jpg',
+      'https://w.wallhaven.cc/full/dg/wallhaven-dg7y23.jpg',
+      'https://w.wallhaven.cc/full/j5/wallhaven-j5zjoq.png',
+      'https://w.wallhaven.cc/full/j5/wallhaven-j5zprq.jpg',
+      'https://w.wallhaven.cc/full/j5/wallhaven-j5zzww.jpg',
+      'https://w.wallhaven.cc/full/ey/wallhaven-eymker.jpg',
+    ];
   }
 
 }
