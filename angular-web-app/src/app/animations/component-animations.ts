1e3a45dfb5d09bd740fb6f16ea128915b088fd38
patricknnn <patrickniewold@gmail.com>
2020-11-09 08:45:13 +0100

    added component animations

--- angular-web-app/src/app/animations/component-animations.ts
+++ angular-web-app/src/app/animations/component-animations.ts
@@ -7,8 +7,10 @@ export const componentAnimations = trigger('scaleInOut', [
     animate(
       '1s cubic-bezier(0.250, 0.460, 0.450, 0.940)',
       keyframes([
-        style({ transform: 'scale(0)', opacity: 1, offset: 0.5 }),
-        style({ transform: 'scale(1)', opacity: 1, offset: 1 }),
+        //style({ flex: '0 0 0%', padding: '0px',  opacity: 0, offset: 0 }),
+        //style({ flex: '*', padding: '*', opacity: 1, offset: 1 }),
+        style({ transform: 'translate3d(0px, 0px, 0px) scale3d(0.001, 0.001, 1)', opacity: 0, offset: 0 }),
+        style({ transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1)', opacity: 1, offset: 1 }),
       ])
     )
   ),
@@ -17,9 +19,15 @@ export const componentAnimations = trigger('scaleInOut', [
     animate(
       '1s cubic-bezier(0.250, 0.460, 0.450, 0.940)',
       keyframes([
-        style({ transform: 'scale(1)', opacity: 1, offset: 0 }),
-        style({ transform: 'scale(0)', opacity: 1, offset: 0.5 }),
+        //style({ flex: '*', padding: '*', opacity: 1, offset: 0 }),
+        //style({ flex: '0 0 0%', padding: '0px', opacity: 0, offset: 1 }),
+        style({ flex: '*', padding: '*', transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1)', opacity: 1, offset: 0 }),
+        style({ flex: '0 0 0%', padding: '0px', transform: 'translate3d(0px, 0px, 0px) scale3d(0.001, 0.001, 1)', opacity: 0, offset: 1 }),
       ])
     )
   ),
 ]);
+
+// transform: 'scaleX(0)', 'transform-origin': '0% 0%',
+// transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1)'
+// translate3d(0px, 0px, 0px) scale3d(0.001, 0.001, 1);
