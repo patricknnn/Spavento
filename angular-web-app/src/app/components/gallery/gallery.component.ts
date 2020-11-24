b1ab2dfe22e7f24903f362b816fde814ac1f2ae9
Patrick Niewold <patrickniewold@gmail.com>
2020-11-24 18:40:52 +0100

    added shufflejs

--- angular-web-app/src/app/components/gallery/gallery.component.ts
+++ angular-web-app/src/app/components/gallery/gallery.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Painting } from '../../models/painting';
 import { PaintingService } from '../../services/painting.service';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { componentAnimations } from '../../animations/component-animations';
+import Shuffle from 'shufflejs';
 
 @Component({
   selector: 'app-gallery',
@@ -11,20 +12,35 @@ import { componentAnimations } from '../../animations/component-animations';
   styleUrls: ['./gallery.component.scss'],
   animations: [componentAnimations],
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnInit, AfterViewInit {
+  @ViewChild('shuffleContainer') private shuffleContainer: ElementRef;
+  @ViewChild('shuffleSizer') private shuffleSizer: ElementRef;
   paintings: Painting[];
   filteredPaintings: Painting[];
   filters = ['TV Series', 'Nature', 'Animals'];
+  private shuffleInstance: Shuffle;
 
+  /**
+   * Constructor
+   * @param paintingService painting service
+   * @param router router
+   * @param modalService modal service
+   */
   constructor(
     private paintingService: PaintingService,
     private router: Router,
     private modalService: NgbModal
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.paintings = this.paintingService.getAllPaintings();
-    this.resetFilters();
+  }
+
+  ngAfterViewInit(): void {
+    this.shuffleInstance = new Shuffle(this.shuffleContainer.nativeElement, {
+      itemSelector: '.shuffle-item',
+      sizer: this.shuffleSizer.nativeElement
+    });
   }
 
   goToPaintingDetails(paintingId: number): void {
@@ -39,14 +55,17 @@ export class GalleryComponent implements OnInit {
   }
 
   resetFilters(): void {
-    this.filteredPaintings = this.paintings;
+    this.shuffleInstance.filter();
   }
 
   setFilter(filter: string): void {
-    this.filteredPaintings = this.paintings.filter((painting) => {
-      if (painting.category == filter) {
-        return true;
-      }
+    // this.filteredPaintings = this.paintings.filter((painting) => {
+    //   if (painting.category == filter) {
+    //     return true;
+    //   }
+    // });
+    this.shuffleInstance.filter((element: Element) => {
+      return element.getAttribute('data-category') == filter;
     });
   }
 }
