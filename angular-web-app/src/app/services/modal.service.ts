e3678f98946ad9ce9d1bfac88c5afcb3670ee9cc
patricknnn <patrickniewold@gmail.com>
2021-01-07 17:13:44 +0100

    fixed modal

--- angular-web-app/src/app/services/modal.service.ts
+++ angular-web-app/src/app/services/modal.service.ts
@@ -7,23 +7,12 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 export class ModalService {
   dialogRef: MatDialogRef<any>;
 
-  constructor(public dialog: MatDialog) {}
+  constructor(
+    public dialog: MatDialog,
+  ) {  }
 
   public openModal(content) {
-    this.beforeModalOpen();
     this.dialogRef = this.dialog.open(content);
-    this.dialogRef.afterClosed().subscribe({
-      complete: this.afterModalClose
-    });
   }
 
-  private beforeModalOpen(): void {
-    let appRoot = document.getElementById('app-root');
-    appRoot.classList.add('app-root-absolute');
-  }
-
-  private afterModalClose(): void {
-    let appRoot = document.getElementById('app-root');
-    appRoot.classList.remove('app-root-absolute');
-  }
 }
