06963d99986968c518da960a5a61c79292469f43
Patrick Niewold <patrickniewold@gmail.com>
2021-03-24 15:41:56 +0100

    bugfixes

--- angular-web-app/src/app/components/_admin/layout-navigation/layout-navigation.component.ts
+++ angular-web-app/src/app/components/_admin/layout-navigation/layout-navigation.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { map } from 'rxjs/operators';
+import { File } from 'src/app/models/file';
 import { GeneralContent } from 'src/app/models/generalcontent';
 import { NavContent } from 'src/app/models/navcontent';
 import { NavLink } from 'src/app/models/navlink';
 import { ContentService } from 'src/app/services/content.service';
+import { FileService } from 'src/app/services/file.service';
 import { SwalService } from 'src/app/services/swal.service';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 
@@ -23,22 +25,43 @@ export class LayoutNavigationComponent implements OnInit {
   navContentHistory: NavContent[];
   formStyle = "standard";
   formColor = "accent";
+  selectedFiles: File[];
 
   constructor(
     private contentService: ContentService,
+    private fileService: FileService,
     private swalService: SwalService
   ) { }
 
   ngOnInit(): void {
+    this.selectedFiles = [];
     this.retrieveData();
     this.retrieveGeneralContent();
   }
 
   onSubmit() {
     this.swalService.loadingSwal("opslaan");
-    this.contentService.saveNavContent(this.navContent).then(() => {
-      this.swalService.successSwal("opgeslagen");
-    });
+    if (this.selectedFiles.length > 0) {
+      let file: File = {
+        id: "",
+        name: "",
+        url: "",
+        timestampCreated: 0,
+        timestampUpdated: 0,
+        file: this.selectedFiles[0],
+      };
+      const { downloadUrl, uploadProgress } = this.fileService.pushFileToStorageAndReturnMetadata(file);
+      downloadUrl.subscribe((downloadUrl) => {
+        this.navContent.brandImage = downloadUrl;
+        this.contentService.saveNavContent(this.navContent).then(() => {
+          this.swalService.successSwal("opgeslagen");
+        });
+      });
+    } else {
+      this.contentService.saveNavContent(this.navContent).then(() => {
+        this.swalService.successSwal("opgeslagen");
+      });
+    }
   }
 
   retrieveData(): void {
@@ -86,6 +109,26 @@ export class LayoutNavigationComponent implements OnInit {
   }
 
   /**
+   * Handles file select
+   * @param event File select event
+   */
+  onFileSelect(event): void {
+    if (this.selectedFiles.length) {
+      this.swalService.errorSwal("Maximaal 1 foto toegestaan")
+    } else {
+      this.selectedFiles.push(...event.addedFiles);
+    }
+  }
+
+  /**
+   * Handles file remove
+   * @param event File remove event
+   */
+  onFileRemove(event): void {
+    this.selectedFiles.splice(this.selectedFiles.indexOf(event), 1);
+  }
+
+  /**
    * Clears form of data
    * Asks user for confirmation
    */
@@ -93,6 +136,7 @@ export class LayoutNavigationComponent implements OnInit {
     this.swalService.promptSwal("Alle veranderingen zullen worden teruggedraaid").then((result) => {
       if (result.value) {
         this.retrieveData();
+        this.selectedFiles = [];
         this.swalService.successSwal("Veranderingen teruggedraaid");
         this.form.form.markAsPristine();
       } else if (result.dismiss === Swal.DismissReason.cancel) {
