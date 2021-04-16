c30e205340d6c15ccd274876a55a0350388c8256
Patrick Niewold <patrickniewold@gmail.com>
2021-04-16 14:11:02 +0200

    updated cms styling

--- angular-web-app/src/app/components/_admin/sidenav/sidenav.component.ts
+++ angular-web-app/src/app/components/_admin/sidenav/sidenav.component.ts
@@ -15,34 +15,34 @@ export class SidenavComponent implements OnInit {
       new Sidenavlink("Dashboard", "dashboard", "/admin/dashboard"),
       new Sidenavlink("Algemeen", "settings_applications", "/admin/general"),
       new Sidenavlink("Beheer", "assignment_turned_in", "", [
-        new Sidenavlink("Bestanden", "library_add_check", "/admin/manager-files"),
-        new Sidenavlink("Gebruikers", "verified_user", "/admin/manager-users"),
+        new Sidenavlink("Bestanden", "folder", "/admin/manager-files"),
+        new Sidenavlink("Gebruikers", "people", "/admin/manager-users"),
         new Sidenavlink("Berichten", "send", "/admin/manager-messages"),
       ]),
       new Sidenavlink("Layout", "view_sidebar", "", [
         new Sidenavlink("Navigatie", "explore", "/admin/layout-navigation"),
-        new Sidenavlink("Header", "panorama", "/admin/layout-header"),
+        new Sidenavlink("Header", "vrpano", "/admin/layout-header"),
         new Sidenavlink("Footer", "copyright", "/admin/layout-footer"),
       ]),
-      new Sidenavlink("Pagina's", "web", "", [
+      new Sidenavlink("Teksten", "wysiwyg", "", [
         new Sidenavlink("Home", "home", "/admin/page-home"),
-        new Sidenavlink("Portfolio", "collections", "/admin/page-portfolio"),
-        new Sidenavlink("Kalender", "collections", "/admin/page-calendar"),
-        new Sidenavlink("Nieuws", "library_books", "/admin/page-news"),
+        new Sidenavlink("Portfolio", "image", "/admin/page-portfolio"),
+        new Sidenavlink("Kalender", "date_range", "/admin/page-calendar"),
+        new Sidenavlink("Nieuws", "feed", "/admin/page-news"),
         new Sidenavlink("Contact", "perm_contact_calendar", "/admin/page-contact"),
         new Sidenavlink("404", "explore_off", "/admin/page-not-found")
       ]),
-      new Sidenavlink("Portfolio", "collections", "", [
-        new Sidenavlink("Overzicht", "library_books", "/admin/portfolio-overview"),
-        new Sidenavlink("Toevoegen", "library_add", "/admin/portfolio-add"),
+      new Sidenavlink("Portfolio", "image", "", [
+        new Sidenavlink("Overzicht", "list", "/admin/portfolio-overview"),
+        new Sidenavlink("Toevoegen", "add", "/admin/portfolio-add"),
       ]),
-      new Sidenavlink("Nieuws", "library_books", "", [
-        new Sidenavlink("Overzicht", "library_books", "/admin/news-overview"),
-        new Sidenavlink("Toevoegen", "library_add", "/admin/news-add"),
+      new Sidenavlink("Kalender", "date_range", "", [
+        new Sidenavlink("Overzicht", "list", "/admin/calendar-overview"),
+        new Sidenavlink("Toevoegen", "add", "/admin/calendar-add"),
       ]),
-      new Sidenavlink("Kalender", "library_books", "", [
-        new Sidenavlink("Overzicht", "library_books", "/admin/calendar-overview"),
-        new Sidenavlink("Toevoegen", "library_add", "/admin/calendar-add"),
+      new Sidenavlink("Nieuws", "feed", "", [
+        new Sidenavlink("Overzicht", "list", "/admin/news-overview"),
+        new Sidenavlink("Toevoegen", "add", "/admin/news-add"),
       ]),
     ];
   }
